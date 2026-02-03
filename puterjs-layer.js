04f336176d75328013c1b65900c41f3acc43c912
ProgrammerIn-wonderland <3838shah@gmail.com>
2026-02-03 15:43:56 -0800

    first commit

---
+++ puterjs-layer.js
@@ -0,0 +1,81 @@
+import * as Comlink from "https://unpkg.com/comlink/dist/esm/comlink.mjs";
+
+Comlink.expose(puter);
+
+window.sharedObject = {};
+
+navigator.serviceWorker.register("./sw.js", {scope: "/"});
+
+async function getFolderPath() {
+    console.log("recieved a request from the heavens to get a file picker, obliging");
+    return (await puter.ui.showDirectoryPicker()).path.replace("~", "/" + (puter.whoami || await puter.getUser()).username);
+}
+async function getFilePath() {
+    return (await puter.ui.showFilePicker()).path.replace("~", "/" + (puter.whoami || await puter.getUser()).username);
+}
+async function ensureAuth() {
+    await puter.ui.auth.signIn();
+    if (!puter.authToken) {
+        await ensureAuth();
+    }
+}
+
+function initComlink() {
+    const channel = new MessageChannel();
+    navigator.serviceWorker.controller.postMessage(channel.port2, [channel.port2]);
+    Comlink.expose(sharedObject, channel.port1);
+    channel.port1.start();
+    sharedObject.hello = "hi";
+    sharedObject.puter = puter;
+    sharedObject.getFolderPath = getFolderPath;
+    sharedObject.getFilePath = getFilePath;
+    sharedObject.ensureAuth = ensureAuth;
+    // console.log("shared object: ", _runInSW);
+}
+
+if (navigator.serviceWorker.controller) {
+    initComlink();
+} else {
+    window.location.reload();
+}
+
+
+window.Comlink = Comlink;
+
+
+navigator.serviceWorker.addEventListener('controllerchange', initComlink);
+
+
+// navigator.serviceWorker.addEventListener("message", (event) => {
+//     const message = event.data;
+//     switch (message.op) {
+//         case 'showFilePicker':
+//             (async () => {
+//                 const fsitem = await puter.ui.showOpenFilePicker();
+//                 const resData = new FormData();
+//                 resData.set("file", fsitem.path);
+//                 resData.set("tag", event.data.tag);
+//                 fetch("/syscalls/respondShowFilePicker", { method: "GET", body: resData });
+//             })();
+
+//             break;
+//         case 'showFolderPicker':
+//             (async () => {
+//                 const fsitem = await puter.ui.showDirectoryPicker();
+//                 const resData = new FormData();
+//                 resData.set("folder", fsitem.path);
+//                 resData.set("tag", event.data.tag);
+
+//                 fetch("/syscalls/respondShowFolderPicker", { method: "GET", body: resData });
+//             })();
+//             break;
+//         case 'getToken': 
+//             (async () => {
+//                 const resData = new FormData();
+//                 resData.set("token", puter.authToken);
+//                 resData.set("tag", event.data.tag);
+//                 fetch("/syscalls/respondGetToken", { method: "GET", body: resData });
+//             })();
+//             break;
+//     }
+// })
---
+++ sw.js
@@ -0,0 +1,53 @@
+importScripts("https://unpkg.com/comlink@4.4.2/dist/umd/comlink.js");
+
+let token = undefined;
+let sharedObject = {};
+let readyPromise = new Promise(res => {
+    self.addEventListener('message', (event) => {
+        if (event.data instanceof MessagePort) {
+            sharedObject = Comlink.wrap(event.data);
+            event.data.start();
+            res();
+        }
+    });
+})
+
+let routes = {
+    GET: {
+        ['/syscall/getFilePicker']: async function (response) {
+            await readyPromise;
+
+        },
+
+        ['/syscall/getFolderPicker']: async function(response) {
+            await readyPromise;
+            console.log("MORTAL's requested folder path");
+            const thing = await sharedObject.getFolderPath();
+            console.log("MORTAL's folder path: ", thing);
+            return new Response(thing);
+        },
+
+        ['/syscall/getToken']: async function (response) {
+            await readyPromise;
+            const token = await sharedObject.puter.authToken;
+            if (!token) {
+                await sharedObject.ensureAuth();
+            }
+            return new Response(await sharedObject.puter.authToken);
+        }
+    }
+}
+
+async function router( {/** @type {Request} */ request} ) {
+    const pathname = new URL(request.url).pathname;
+    return routes[request.method][pathname](request);
+}
+
+self.addEventListener("fetch", ( /** @type {FetchEvent} */event) => {
+    if (new URL(event.request.url).pathname.startsWith("/syscall/"))
+        event.respondWith(router(event));
+})
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(clients.claim());
+});
